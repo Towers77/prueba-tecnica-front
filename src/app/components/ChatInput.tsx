a5002f1095f503d033e2eb67d9c2f19a07260977
Torres <santitorres460@mi.unc.edu.ar>
2025-05-17 19:13:45 -0300

    ChatInput almost done

---
+++ src/app/components/ChatInput.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { useLayoutEffect, useRef, useState } from 'react';
+import { useChatContext } from '../utils/chatContext';
+
+type ChatInputProps = {
+	action: (payload: FormData) => void;
+};
+
+function ChatInput({ action }: ChatInputProps) {
+	const { messages, setMessages } = useChatContext();
+	const [query, setQuery] = useState<string>('');
+
+	const textAreaRef = useRef<HTMLTextAreaElement>(null);
+	useLayoutEffect(() => {
+		const textArea =
+			textAreaRef.current ??
+			(document.getElementById('query') as HTMLTextAreaElement);
+
+		if (textArea) {
+			textArea.style.height = '0px';
+			const scrollHeight = textArea.scrollHeight;
+			textArea.style.height = `${scrollHeight}px`;
+		}
+	}, [textAreaRef, query]);
+
+	const handleSubmit = () => {
+		if (query.trim() === '') return;
+		setMessages((prev) => [...prev, { text: query.trim(), isFromUser: true }]);
+		console.log(messages);
+	};
+
+	return (
+		<form
+			action={action}
+			className="bg-zinc-800 rounded-2xl w-full flex gap-4 items-center py-4 outline-zinc-600 outline-1 shadow-lg"
+		>
+			<textarea
+				onChange={(e) => setQuery(e.target.value)}
+				className="w-full text-sm placeholder:text-zinc-400 px-6 scrollbar-fancy focus:outline-none resize-none max-h-[120px]"
+				ref={textAreaRef}
+				placeholder="Ask anything..."
+				id="query"
+				name="query"
+				value={query}
+				required
+			/>
+			<button
+				className="bg-zinc-900 px-4 py-2 flex items-center text-sm rounded-xl mr-3 hover:scale-105 hover:shadow-lg transition-all duration-200 cursor-pointer"
+				type="submit"
+				onClick={handleSubmit}
+			>
+				Send
+			</button>
+		</form>
+	);
+}
+
+export default ChatInput;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-import Image from 'next/image';
+'use client';
+
+import { useActionState } from 'react';
 import ChatInput from './components/ChatInput';
+import ChatContextProvider from './utils/chatContext';
+import { getQueryResponse } from './utils/actions';
 
 export default function Home() {
+	const [state, action, isLoading] = useActionState(getQueryResponse, '');
+
 	return (
-		<main className="h-full flex flex-col justify-center">Hello World</main>
+		<main className="h-full w-2/3 lg:w-1/3 flex flex-col justify-end py-8">
+			<ChatContextProvider>
+				<ChatInput action={action} />
+			</ChatContextProvider>
+		</main>
 	);
 }
