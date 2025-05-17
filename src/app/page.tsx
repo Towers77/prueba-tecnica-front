5b57d893cdaf4ea3af0db6835541cf814b17c81f
Torres <santitorres460@mi.unc.edu.ar>
2025-05-17 16:49:04 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image';
+import ChatInput from './components/ChatInput';
+
+export default function Home() {
+	return (
+		<main className="h-full flex flex-col justify-center">Hello World</main>
+	);
+}
