4a478ecdb5ea524598d03aebdf01866586c3d1b8
Torres <santitorres460@mi.unc.edu.ar>
2025-05-17 22:33:31 -0300

    Refactor Home page and separated Chat from it

---
+++ src/app/components/Chat.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { useChatContext } from '../utils/chatContext';
+import ChatBubble from './ChatBubble';
+
+type ChatProps = {
+	isLoading: boolean;
+};
+
+function Chat({ isLoading }: ChatProps) {
+	const { messages, setMessages } = useChatContext();
+
+	const chatRef = useRef<HTMLDivElement>(null);
+	useEffect(() => {
+		if (chatRef.current) {
+			chatRef.current.scrollTop = chatRef.current.scrollHeight;
+		}
+	}, [messages, isLoading]);
+
+	return (
+		<div
+			ref={chatRef}
+			className="flex flex-col gap-4 overflow-y-scroll scrollbar-fancy px-2 overflow-x-hidden"
+		>
+			{messages.map((message, index) => (
+				<ChatBubble
+					key={index}
+					text={message.text}
+					isFromUser={message.isFromUser}
+				/>
+			))}
+			{isLoading && <div className="animate-pulse">Thinking...</div>}
+		</div>
+	);
+}
+
+export default Chat;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,14 @@
 'use client';
 
-import { useActionState, useEffect, useRef } from 'react';
+import { useActionState, useEffect } from 'react';
 import ChatInput from './components/ChatInput';
 import { useChatContext } from './utils/chatContext';
 import { getQueryResponse } from './utils/actions';
-import ChatBubble from './components/ChatBubble';
+import Chat from './components/Chat';
 
 export default function Home() {
-	const chatRef = useRef<HTMLDivElement>(null);
 	const [state, action, isLoading] = useActionState(getQueryResponse, '');
-	const { messages, setMessages } = useChatContext();
-
-	useEffect(() => {
-		if (chatRef.current) {
-			chatRef.current.scrollTop = chatRef.current.scrollHeight;
-		}
-	}, [messages, state, isLoading]);
+	const { setMessages } = useChatContext();
 
 	useEffect(() => {
 		if (state) {
@@ -24,20 +17,8 @@ export default function Home() {
 	}, [state]);
 
 	return (
-		<main className="h-full w-2/3 lg:w-1/3 flex flex-col justify-end py-8 gap-4">
-			<div
-				ref={chatRef}
-				className="flex flex-col gap-4 overflow-y-scroll scrollbar-fancy px-2"
-			>
-				{messages.map((message, index) => (
-					<ChatBubble
-						key={index}
-						text={message.text}
-						isFromUser={message.isFromUser}
-					/>
-				))}
-				{isLoading && <div className="animate-pulse">Thinking...</div>}
-			</div>
+		<main className="h-full w-full mx-4 md:w-2/3 lg:w-1/2 flex flex-col justify-end py-4 md:py-8 gap-4">
+			<Chat isLoading={isLoading} />
 			<ChatInput action={action} isLoading={isLoading} />
 		</main>
 	);
