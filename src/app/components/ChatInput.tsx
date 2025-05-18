83813c05fd6a1f7b49a7528720b2d8d0280435ec
Torres <santitorres460@mi.unc.edu.ar>
2025-05-18 13:02:59 -0300

    Separated The resizable text are into a new component

--- src/app/components/ChatInput.tsx
+++ src/app/components/ChatInput.tsx
@@ -1,30 +1,17 @@
 'use client';
 
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { useChatContext } from '../utils/chatContext';
+import { useChatContext } from '../context/chatContext';
+import ResizableTextArea from './ResizableTextArea';
 
 type ChatInputProps = {
-	action: (payload: FormData) => void;
-	isLoading: boolean;
+	onSubmitAction: (payload: FormData) => void;
+	isDisabled: boolean;
 };
 
-function ChatInput({ action, isLoading }: ChatInputProps) {
+function ChatInput({ onSubmitAction, isDisabled }: ChatInputProps) {
 	const { setMessages } = useChatContext();
 	const [query, setQuery] = useState<string>('');
-
-	// TODO: Move to a custom hook
-	const textAreaRef = useRef<HTMLTextAreaElement>(null);
-	useLayoutEffect(() => {
-		const textArea =
-			textAreaRef.current ??
-			(document.getElementById('query') as HTMLTextAreaElement);
-
-		if (textArea) {
-			textArea.style.height = '0px';
-			const scrollHeight = textArea.scrollHeight;
-			textArea.style.height = `${scrollHeight}px`;
-		}
-	}, [textAreaRef, query]);
 
 	const handleSubmit = () => {
 		if (query.trim() === '') return;
@@ -32,31 +19,26 @@ function ChatInput({ action, isLoading }: ChatInputProps) {
 	};
 
 	useEffect(() => {
-		if (!isLoading) {
+		if (!isDisabled) {
 			setQuery('');
 		}
-	}, [isLoading]);
+	}, [isDisabled]);
 
 	return (
 		<form
-			action={action}
+			action={onSubmitAction}
 			className="bg-zinc-800 rounded-2xl w-full flex gap-4 items-center py-4 outline-zinc-600 outline-1 shadow-lg"
 		>
-			<textarea
+			<ResizableTextArea
 				onChange={(e) => setQuery(e.target.value)}
-				className="w-full text-sm placeholder:text-zinc-400 px-6 scrollbar-fancy focus:outline-none resize-none max-h-[120px]"
-				ref={textAreaRef}
-				placeholder="Ask anything..."
-				id="query"
-				name="query"
-				value={isLoading ? '' : query}
-				disabled={isLoading}
+				value={query}
+				isDisabled={isDisabled}
 			/>
 			<button
 				className="bg-zinc-900 px-4 py-2 flex items-center text-sm rounded-xl mr-3 hover:scale-105 hover:shadow-lg active:scale-100 disabled:text-zinc-800 disabled:cursor-auto disabled:scale-100 transition-all duration-200 cursor-pointer"
 				type="submit"
 				onClick={handleSubmit}
-				disabled={isLoading}
+				disabled={isDisabled}
 			>
 				Send
 			</button>
---
+++ src/app/components/ResizableTextArea.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { ChangeEvent, useLayoutEffect, useRef } from 'react';
+
+type ResizableTextAreaProps = {
+	onChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+	value: string;
+	isDisabled: boolean;
+};
+
+function ResizableTextArea({
+	onChange,
+	value,
+	isDisabled,
+}: ResizableTextAreaProps) {
+	const textAreaRef = useRef<HTMLTextAreaElement>(null);
+	useLayoutEffect(() => {
+		const textArea =
+			textAreaRef.current ??
+			(document.getElementById('query') as HTMLTextAreaElement);
+
+		if (textArea) {
+			textArea.style.height = '0px';
+			const scrollHeight = textArea.scrollHeight;
+			textArea.style.height = `${scrollHeight}px`;
+		}
+	}, [textAreaRef, value]);
+
+	return (
+		<textarea
+			onChange={onChange}
+			className="w-full text-sm placeholder:text-zinc-400 px-6 scrollbar-fancy focus:outline-none resize-none max-h-[120px]"
+			ref={textAreaRef}
+			placeholder="Ask anything..."
+			id="query"
+			name="query"
+			disabled={isDisabled}
+		/>
+	);
+}
+
+export default ResizableTextArea;
